40a52fd234d0facc90ecce7dbbd4c85d5b88c11b
mzetkulic <mattzetkulic@gmail.com>
2017-09-13 17:27:19 -0400

    Downloading dataset

---
+++ createDataset.R
@@ -0,0 +1,18 @@
+library(readr)
+
+cleveland <- read_csv(file="processed.cleveland.data.csv",col_names=FALSE)
+swiss <- read_csv(file="processed.hungarian.data.csv",col_names=FALSE)
+va <- read_csv(file="processed.va.data.csv",col_names=FALSE)
+hungary <- read_csv(file="processed.switzerland.data.csv",col_names=FALSE)
+
+namesvec <- c("age","sex","cp","trestbps","chol","fbs","restecg","thalach","exang"    
+                      ,"oldpeak"  
+                      ,"slope"  
+                      ,"ca"        
+                      ,"thal"    
+                      ,"num")
+
+names(cleveland) <- namesvec
+names(swiss) <- namesvec
+names(va) <- namesvec
+names(hungary) <- namesvec
