aa423b8a8b9ea67ecd7ee44ff90522666cd65adf
mzetkulic <mattzetkulic@gmail.com>
2017-09-14 10:48:30 -0400

    Random Forest

--- createDataset.R
+++ createDataset.R
@@ -1,3 +1,12 @@
+# Thank  you to the following researchers for the data:
+#   
+#   1. Hungarian Institute of Cardiology. Budapest: Andras Janosi, M.D.
+# 2. University Hospital, Zurich, Switzerland: William Steinbrunn, M.D.
+# 3. University Hospital, Basel, Switzerland: Matthias Pfisterer, M.D.
+# 4. V.A. Medical Center, Long Beach and Cleveland Clinic Foundation:
+#   Robert Detrano, M.D., Ph.D.
+
+
 library(readr)
 library(dplyr)
 library(ISwR)
@@ -26,3 +35,18 @@ names(hungary) <- namesvec
 
 all <- rbind(cleveland,swiss,va,hungary)
 
+allclean <- all %>% filter(!(age=="?")) %>% filter(!(sex=="?")) %>% filter(!(trestbps=="?")) %>% filter(!(num=="?"))
+# removing "?" values for relevant columns
+
+allclean <- allclean %>% filter(!(chol=="?"))
+
+allclean <- transform(allclean, trestbps = as.numeric(chol))
+allclean <- transform(allclean, age = as.numeric(age))
+allclean <- transform(allclean, sex = as.numeric(sex))
+allclean <- transform(allclean, chol = as.numeric(chol))
+fit <- rpart(num ~ chol,data=allclean,method="class")
+
+library(randomForest)
+fit <- randomForest(num ~ chol + age + trestbps + sex,data=allclean)
+print(fit) # view results 
+importance(fit) # importance of each predictor
